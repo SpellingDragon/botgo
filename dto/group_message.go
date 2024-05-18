6508ace7c9eb0024ce010634de6a31ceab1f7ebd
weiyepeng <weiyepeng@tencent.com>
2024-05-19 01:10:42 +0800

    feat:merge

--- dto/group_message.go
+++ dto/group_message.go
@@ -4,15 +4,6 @@ import "time"
 
 // GroupMessage 消息结构体定义
 type GroupMessage struct {
-	Op int    `json:"op"`
-	S  int    `json:"s"`
-	T  string `json:"t"`
-	ID string `json:"id"`
-	D  Data   `json:"d"`
-}
-
-// Data represents the data part of the websocket message.
-type Data struct {
 	Author      Author    `json:"author"`
 	Content     string    `json:"content"`
 	GroupOpenID string    `json:"group_openid"`
